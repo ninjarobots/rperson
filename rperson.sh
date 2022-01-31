e12037b0c89cb9206d122a59ec2a1b6e0bfd5243
James <jlove7714@gmail.com>
2022-01-31 12:46:01 -0500

    Making directory path if it doesnt exist

--- rperson.sh
+++ rperson.sh
@@ -4,6 +4,6 @@ person=$(curl -s https://api.justyy.workers.dev/api/rig)
 mod_person=$(echo $person | sed 's/"//g')
 fname=$(echo $mod_person sed 's/\n/\/g'| cut -d '\' -f 1 | sed 's/ /_/g')
 pdir=$( echo $0 | sed 's/rperson.sh//g')
-mkdir "$pdir"people/$fname
+mkdir -p "$pdir"people/$fname
 echo -ne $mod_person > "$pdir"people/$fname/info
-wget -q -nd -O "$pdir"people/"$fname"/"$fname".jpeg -A jpeg https://thispersondoesnotexist.com/image 
+wget -q -nd -O "$pdir"people/"$fname"/"$fname".jpeg -A jpeg https://thispersondoesnotexist.com/image
